721f898fe2756040d6a54285030461b1625f4a30
Protorians <protorians.dev@gmail.com>
2024-11-01 23:39:50 +0000

    Fix flex ruleKit

--- source/rules/flex.ts
+++ source/rules/flex.ts
@@ -4,10 +4,28 @@ export function flexboxRuleKit() {
   return new RuleKit({
     alias: 'flex',
     transform({value}) {
-      return {
-        display: "flex",
-        flexDirection: `${value || 'row'}`,
-      };
+      switch (value.trim().toLowerCase()) {
+
+        case 'fill':
+          return {flex: '1 1 auto'}
+
+        case 'center':
+          return {justifyContent: 'center', alignItems: 'center',}
+
+        case 'wrap':
+        case 'no-wrap':
+          return {flexWrap: value,}
+
+        case 'row':
+        case 'row-reverse':
+        case 'column':
+        case 'column-reverse':
+          return {display: "flex", flexDirection: value,}
+
+        default:
+          return {}
+
+      }
     }
   })
 }
