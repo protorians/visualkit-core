55a53c0c76119e849d6ff43e50ca1a66ebaf3701
Protorians <protorians.dev@gmail.com>
2024-11-01 23:41:58 +0000

    Add ucFirst ans ucWords

--- source/utils/string.ts
+++ source/utils/string.ts
@@ -1,22 +1,28 @@
-
-
-export function trimSpace(data: string){
+export function trimSpace(data: string) {
   return data.replace(/ /g, '')
 }
 
-export function randomName(length: number = 10){
+export function ucFirst(data: string, strict: boolean = false): string {
+  return data.charAt(0).toUpperCase() + (strict ? data.slice(1).toLowerCase() : data.slice(1));
+}
+
+export function ucWords(data: string, strict: boolean = false): string {
+  return data.split(' ').map(frag => frag.charAt(0).toUpperCase() + (strict ? frag.slice(1).toLowerCase() : frag.slice(1))).join(' ');
+}
+
+export function randomName(length: number = 10) {
   const provider = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let out: string[] = [];
 
   for (let index = 0; index < length; index++) {
-    out.push( provider.charAt(Math.floor(Math.random() * provider.length)) )
+    out.push(provider.charAt(Math.floor(Math.random() * provider.length)))
   }
 
   return out.join('');
 }
 
 
-export function logTime(date?: Date){
+export function logTime(date?: Date) {
   return (date || (new Date())).toLocaleTimeString('en-US', {
     hour: 'numeric',
     minute: 'numeric',
@@ -25,7 +31,7 @@ export function logTime(date?: Date){
   });
 }
 
-export function logDateTime(date?: Date){
+export function logDateTime(date?: Date) {
   return (date || (new Date())).toLocaleTimeString('en-US', {
     year: 'numeric',
     month: 'numeric',
