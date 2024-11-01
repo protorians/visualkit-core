14a8da6a5611f636f986486ef248185b327295b5
Protorians <protorians.dev@gmail.com>
2024-11-01 23:40:33 +0000

    Fix padding ruleKit

--- source/rules/padding.ts
+++ source/rules/padding.ts
@@ -8,3 +8,77 @@ export function paddingRuleKit() {
     }
   })
 }
+
+export function paddingXRuleKit() {
+  return new RuleKit({
+    alias: 'p-x',
+    transform({value}) {
+      const val = RulesKit.getSize(value) || value
+      return {
+        paddingLeft: val,
+        paddingRight: val,
+      };
+    }
+  })
+}
+
+export function paddingYRuleKit() {
+  return new RuleKit({
+    alias: 'p-y',
+    transform({value}) {
+      const val = RulesKit.getSize(value) || value
+      return {
+        paddingTop: val,
+        paddingBottom: val,
+      };
+    }
+  })
+}
+
+export function paddingTopRuleKit() {
+  return new RuleKit({
+    alias: 'p-t',
+    transform({value}) {
+      const val = RulesKit.getSize(value) || value
+      return {
+        paddingTop: val,
+      };
+    }
+  })
+}
+
+export function paddingRightRuleKit() {
+  return new RuleKit({
+    alias: 'p-r',
+    transform({value}) {
+      const val = RulesKit.getSize(value) || value
+      return {
+        paddingRight: val,
+      };
+    }
+  })
+}
+
+export function paddingBottomRuleKit() {
+  return new RuleKit({
+    alias: 'p-b',
+    transform({value}) {
+      const val = RulesKit.getSize(value) || value
+      return {
+        paddingBottom: val,
+      };
+    }
+  })
+}
+
+export function paddingLeftRuleKit() {
+  return new RuleKit({
+    alias: 'p-l',
+    transform({value}) {
+      const val = RulesKit.getSize(value) || value
+      return {
+        paddingLeft: val,
+      };
+    }
+  })
+}
