f488c4f54ed014faaab2d6944571c7b4c9f7bc9c
Protorians <protorians.dev@gmail.com>
2024-11-01 23:44:14 +0000

    Fix required importing and Add begin and close methods to excavation

--- source/plugin/index.ts
+++ source/plugin/index.ts
@@ -1,5 +1,5 @@
 import * as process from "process"
-import {ConfigKit, ExcavationKit,} from "../foundation";
+import {BuilderKit, ConfigKit, ExcavationKit,} from "../foundation";
 import {Logger} from "../utils/logger";
 import {RaitonPlugin} from "./raiton";
 
@@ -23,39 +23,17 @@ export function visualkit() {
     },
 
     transformIndexHtml(html: string) {
-
-      // <script type="module" src="${pkgRoot}${path.sep}..${path.sep}library${path.sep}appearance.scss"></script>
-      // <script type="module" src="${pkgRoot}${path.sep}main.js"></script>
-
       return html.replace(
         '</head>',
         `
-        <link rel="stylesheet" href="/${RaitonPlugin.cachesDir}/globals.scss" />
-        <link rel="stylesheet" href="/${RaitonPlugin.cachesDir}/theme.scss" />
-        <script type="module" src="/${RaitonPlugin.cachesDir}/main.js" ></script>
-        </head>`
+    <link rel="stylesheet" href="/${RaitonPlugin.cachesDir}/globals.scss" /><link rel="stylesheet" href="/${RaitonPlugin.cachesDir}/theme.scss" /><script type="module" src="/${RaitonPlugin.cachesDir}/main.js" ></script>
+</head>
+`
       );
     },
 
-    // buildStart() {
-    //   Logger.notice('BuildStart', 'Processing');
-    // },
-    //
-    // handleHotUpdate() {
-    //   Logger.notice('handleHotUpdate', 'Processing');
-    // },
-    //
-    // buildEnd() {
-    //   Logger.notice('buildEnd', 'Processing');
-    // },
-
     transform(src: string, id: string) {
-      const root = ConfigKit.schematic.build?.directories?.root || process.cwd()
-      // const isScript = id.endsWith('.js') || id.endsWith('.ts') || id.endsWith('.jsx') || id.endsWith('.tsx')
-
-      // if (!configLoaded && isScript) {
-      //   src = `${plugin.required()}\n${src}`;
-      // }
+      const root = ConfigKit.schematic.build?.directories?.root || process.cwd();
 
       if (ConfigKit.schematic.rules && !id.endsWith(`${RaitonPlugin.configFilename}`)) {
 
@@ -63,10 +41,11 @@ export function visualkit() {
         if (excavation.accepted) {
           if (id.endsWith('.js') || id.endsWith('.ts') || id.endsWith('.json')) excavation.source(src);
           if (id.endsWith('.html') || id.endsWith('.htm') || id.endsWith('.txt') || id.endsWith('.svg')) excavation.htmlSource(src);
-          excavation.make()
+
+          excavation.begin().make().close()
 
           if (ConfigKit.schematic.build?.autoImports && id.startsWith(root) && excavation.builder.exported) {
-            const generatedFilePath = excavation.builder.getExportFilename(id);
+            const generatedFilePath = BuilderKit.getExportFilename(id);
             const source = generatedFilePath.substring(root.length)
             const inject = `const vkStyle = document.createElement('link');
               vkStyle.rel = 'StyleSheet';
