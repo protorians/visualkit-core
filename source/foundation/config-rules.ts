8a424a73c17b9d4105701e786cc9f50619b90799
Protorians <protorians.dev@gmail.com>
2024-11-01 23:39:05 +0000

    Add primary rules

--- source/foundation/config-rules.ts
+++ source/foundation/config-rules.ts
@@ -1,5 +1,13 @@
 import type {IRuleKit} from "../types";
-import {paddingRuleKit} from "../rules/padding";
+import {
+  paddingBottomRuleKit,
+  paddingLeftRuleKit,
+  paddingRightRuleKit,
+  paddingRuleKit,
+  paddingTopRuleKit,
+  paddingXRuleKit,
+  paddingYRuleKit
+} from "../rules/padding";
 import {alignContentRuleKit, alignItemsRuleKit} from "../rules/align";
 import {justifyContentRuleKit, justifyItemsRuleKit} from "../rules/justify";
 import {foreColorRuleKit} from "../rules/fore";
@@ -14,11 +22,41 @@ import {
 } from "../rules/breakpoints";
 import {flexboxRuleKit} from "../rules/flex";
 import {columnsRuleKit} from "../rules/column";
+import {
+  marginBottomRuleKit,
+  marginLeftRuleKit,
+  marginRightRuleKit,
+  marginRuleKit,
+  marginTopRuleKit,
+  marginXRuleKit,
+  marginYRuleKit
+} from "../rules/margin";
+import {rowRuleKit} from "../rules/row";
+import {fitObjectRuleKit} from "../rules/fit";
+import {heightRuleKit} from "../rules/height";
+import {widthRuleKit} from "../rules/width";
+import {opacityRuleKit} from "../rules/opacity";
+import {ratioRuleKit} from "../rules/ratio";
+import {borderRadiusRuleKit} from "../rules/rounded";
+import {textRuleKit} from "../rules/text";
 
 export function baseRulesKit(): IRuleKit[] {
   return [
     flexboxRuleKit(),
     paddingRuleKit(),
+    paddingXRuleKit(),
+    paddingYRuleKit(),
+    paddingTopRuleKit(),
+    paddingRightRuleKit(),
+    paddingBottomRuleKit(),
+    paddingLeftRuleKit(),
+    marginRuleKit(),
+    marginXRuleKit(),
+    marginYRuleKit(),
+    marginTopRuleKit(),
+    marginRightRuleKit(),
+    marginBottomRuleKit(),
+    marginLeftRuleKit(),
     alignItemsRuleKit(),
     alignContentRuleKit(),
     justifyContentRuleKit(),
@@ -26,6 +64,14 @@ export function baseRulesKit(): IRuleKit[] {
     foreColorRuleKit(),
     backColorRuleKit(),
     columnsRuleKit(),
+    rowRuleKit(),
+    fitObjectRuleKit(),
+    heightRuleKit(),
+    widthRuleKit(),
+    opacityRuleKit(),
+    ratioRuleKit(),
+    borderRadiusRuleKit(),
+    textRuleKit(),
 
     nsMiniPhoneRuleKit(),
     nsPhoneRuleKit(),
